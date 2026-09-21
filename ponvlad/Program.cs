0c7f86eb3c8bcb60938d9c666ae57dcb23f35a56
tyubela <guthubzs10@gmail.com>
2026-09-21 12:41:07 +0200

    wylosowanie i wypisywanie tablicy

--- ponvlad/Program.cs
+++ ponvlad/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Pidar!");
+// tablica 6 wartosci losowych 
+//funkcja zwrata tablice a przekazujemy do niej liczbe elementow
+
+int[] wylosujtablice(int liczbaElementow)
+{
+    int[] tablicawylosowana = new int[liczbaElementow];
+    Random random = new Random();
+    for (int i = 0;i < tablicawylosowana.Length; i++)
+    {
+        tablicawylosowana[i] = random.Next(1, 100);
+    }
+    return tablicawylosowana;
+}
+void wypiszTablice(int[] tablicaDoWypisanie)
+{
+    Console.WriteLine("Wylosowana tablica: ");
+    for (int i = 0; i < tablicaDoWypisanie.Length; i++)
+    {
+        Console.Write(tablicaDoWypisanie[i] + ", ");
+    }
+}
+int[] tablicatestowa = wylosujtablice(10);
+wypiszTablice(tablicatestowa);
